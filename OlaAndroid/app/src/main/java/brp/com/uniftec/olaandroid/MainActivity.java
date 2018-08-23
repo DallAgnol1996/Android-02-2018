7c4ea94f32dc0f66b33e3cdf6776976f10d38d5f
DallAgnol1996 <matheusd25@gmail.com>
2018-08-23 20:57:56 -0300

    Android_Aula_1_2018
    Android_Aula_1_2018

---
+++ OlaAndroid/app/src/main/java/brp/com/uniftec/olaandroid/MainActivity.java
@@ -0,0 +1,20 @@
+package brp.com.uniftec.olaandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nomeEditView;
+    private Button;
+    private EditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
